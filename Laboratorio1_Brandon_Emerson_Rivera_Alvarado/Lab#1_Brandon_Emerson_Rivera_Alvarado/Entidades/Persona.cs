ecf56b67f6fc8ac81b016f40e23eb08a9cb37723
Brandon Emerson <111790942+Brandon2003-dev@users.noreply.github.com>
2023-03-01 23:18:05 -0600

    Add files via upload

---
+++ Laboratorio1_Brandon_Emerson_Rivera_Alvarado/Lab#1_Brandon_Emerson_Rivera_Alvarado/Entidades/Persona.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Brandon_Emerson_Rivera_Alvarado.Entidades
+{
+    public class Persona
+    {
+    public string Nombre { get; set; } 
+    public int Edad { get; set; }
+    public string Sexo { get; set;}
+    public int Peso { get; set;}
+    public double Altura { get; set;}
+    }
+}
